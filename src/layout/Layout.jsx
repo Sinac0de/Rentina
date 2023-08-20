2ff3000f299550335f7412b4bf9e5f028936ca03
Sina Moradian <sinacsharp@gmail.com>
2023-08-21 00:00:33 +0330

    Add footer to the Layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Layout = () => {
   return (
@@ -8,6 +9,7 @@ const Layout = () => {
       <main className="bg-[#F6F7F9] min-h-screen">
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 };
