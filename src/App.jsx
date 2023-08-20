a3519956cfaa1872c14f73ef957423015224a2f7
Sina Moradian <sinacsharp@gmail.com>
2023-08-20 12:54:14 +0330

    Setup react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+import Home from "./pages/Home";
 
 function App() {
-  return <></>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(<Route path="/" element={<Home />} />)
+  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
