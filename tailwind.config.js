c720d9e6476cd21929239609dae9488d1aa95fd9
Sina Moradian <sinacsharp@gmail.com>
2023-08-20 12:53:45 +0330

    Add primary & secondary bg colors and also Add Jakarta font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: {
+          100: "#D6E4FD",
+          200: "#AEC8FC",
+          300: "#85A8F8",
+          400: "#658DF1",
+          500: "#3563E9",
+          600: "#264BC8",
+          700: "#1A37A7",
+          800: "#102587",
+          900: "#0A196F",
+        },
+        secondary: {
+          100: "#E0E9F4",
+          200: "#C3D4E9",
+          300: "#90A3BF",
+          400: "#596780",
+          500: "#1A202C",
+          600: "#131825",
+          700: "#0D121F",
+          800: "#080C19",
+          900: "#040815",
+        },
+      },
+      fontFamily: {
+        jakarta: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
