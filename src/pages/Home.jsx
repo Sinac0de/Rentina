cdaf78524d75bdef8b6b4b00491d6c5759b5a355
Sina Moradian <sinacsharp@gmail.com>
2023-08-22 16:43:02 +0330

    Add Recommended and Popular Cars structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import PicIcon from "../components/Icons/PicIcon";
 import PickDrop from "../components/PickDrop/PickDrop";
+import Slider from "../components/Slider/Slider";
 
 const Home = () => {
   return (
@@ -15,6 +16,25 @@ const Home = () => {
       <section className="p-5">
         {/* Pick - Drop */}
         <PickDrop />
+        {/* Popular cars slider */}
+        <div>
+          <Slider title={"Popular cars"} />
+        </div>
+        {/* Recommended cars*/}
+
+        <div>
+          <header>Recommended cars</header>
+          {/* recommended car cards */}
+          <div>{}</div>
+          <footer className="flex justify-center items-center relative">
+            <button className="bg-primary-500 py-2 px-[20px] rounded-[4px] text-[12px]">
+              Show More Cars
+            </button>
+            <span className="font-bold text-secondary-300 text-[14px] absolute right-5">
+              120 cars
+            </span>
+          </footer>
+        </div>
       </section>
     </>
   );
