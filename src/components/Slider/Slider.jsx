8a5ba6f68534ff264be427d4edbaf63d49838ab3
Sina Moradian <sinacsharp@gmail.com>
2023-08-22 23:45:59 +0330

    Add Fake test Slides to the Slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,16 +1,23 @@
 import { Link } from "react-router-dom";
+import CarCard from "../CarCard/CarCard";
 
 const Slider = ({ title }) => {
   return (
     <section>
-      <header className="flex justify-between">
+      <header className="flex justify-between mb-5">
         <h3>{title}</h3>
         <Link to="." className="text-primary-500 font-semibold">
           <p>View all</p>
         </Link>
       </header>
       {/* slides */}
-      <div>{}</div>
+      <div className="flex overflow-auto gap-5 relative">
+        <CarCard isSlideCard={true} />
+        <CarCard isSlideCard={true} />
+        <CarCard isSlideCard={true} />
+        <CarCard isSlideCard={true} />
+        <CarCard isSlideCard={true} />
+      </div>
     </section>
   );
 };
