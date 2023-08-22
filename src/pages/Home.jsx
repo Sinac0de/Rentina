3d617bf793922e5aab6939bd0ed88c27d2e3a001
Sina Moradian <sinacsharp@gmail.com>
2023-08-22 15:37:34 +0330

    Create base structure of the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,23 @@
+import PicIcon from "../components/Icons/PicIcon";
+import PickDrop from "../components/PickDrop/PickDrop";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      {/* Hero */}
+      <div className="p-5">
+        <div className="bg-white w-full h-52 border rounded-lg flex justify-center items-center">
+          <PicIcon />
+        </div>
+      </div>
+
+      {/* Content */}
+      <section className="p-5">
+        {/* Pick - Drop */}
+        <PickDrop />
+      </section>
+    </>
+  );
 };
 
 export default Home;
