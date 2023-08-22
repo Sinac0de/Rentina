f82e3ac4598ed814d24968d33a71e23b7a2cd287
Sina Moradian <sinacsharp@gmail.com>
2023-08-22 15:35:01 +0330

    Create Bullet, Pic, and PickUp-DropOff icons

---
+++ src/components/Icons/PickDropIcon.jsx
@@ -0,0 +1,42 @@
+const PickDropIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.16026 3.83632L7.16026 17.4541"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.08273 7.93262L7.1605 3.83595L11.2383 7.93262"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M17.089 20.167L17.089 6.54921"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M21.1665 16.0713L17.0887 20.168L13.0109 16.0713"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default PickDropIcon;
