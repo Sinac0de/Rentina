a63f430ec5af3ab8c3ee60914c60ebc56595cb36
Sina Moradian <sinacsharp@gmail.com>
2023-08-22 23:50:24 +0330

    Add some fake height & width to test the section

--- src/components/PickDrop/PickDrop.jsx
+++ src/components/PickDrop/PickDrop.jsx
@@ -2,16 +2,16 @@ import PickDropIcon from "../Icons/PickDropIcon";
 
 const PickDrop = () => {
   return (
-    <section className="flex flex-col items-center">
+    <section className="flex flex-col items-center my-5">
       {/* Pick Up */}
-      <div>Pick Up</div>
+      <div className="w-full h-36 bg-white rounded-lg">Pick Up</div>
 
       <button className="bg-primary-500 rounded-lg w-fit p-4">
         <PickDropIcon />
       </button>
 
       {/* Drop off*/}
-      <div>Drop off</div>
+      <div className="w-full h-36 bg-white rounded-lg">Drop off</div>
     </section>
   );
 };
