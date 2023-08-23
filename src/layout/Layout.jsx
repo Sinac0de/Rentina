2eab2fc13d8f16657dbda63e9fab28335b0afd12
Sina Moradian <sinacsharp@gmail.com>
2023-08-23 23:53:34 +0330

    Add NavBar and navCollapse state to the layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import { Outlet } from "react-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import NavBar from "../components/Sidebar/NavBar";
 
 const Layout = () => {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(false);
+
   return (
     <>
-      <Header />
-      <main className="min-h-screen">
-        <Outlet />
-      </main>
-      <Footer />
+      <NavBar
+        isNavCollapsed={isNavCollapsed}
+        setIsNavCollapsed={setIsNavCollapsed}
+      />
+      <div
+        className={`${!isNavCollapsed ? "fixed right-0 left-0 blur-lg" : ""}`}
+        onClick={() => !isNavCollapsed && setIsNavCollapsed(true)}
+      >
+        <Header setIsNavCollapsed={setIsNavCollapsed} />
+        <main className="min-h-screen">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </>
   );
 };
