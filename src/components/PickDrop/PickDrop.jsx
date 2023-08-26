e22be81a321093f5aed190ae7c7f374254fa9dff
Sina Moradian <sinacsharp@gmail.com>
2023-08-26 14:20:49 +0330

    Add shadow to the button

--- src/components/PickDrop/PickDrop.jsx
+++ src/components/PickDrop/PickDrop.jsx
@@ -4,14 +4,15 @@ const PickDrop = () => {
   return (
     <section className="flex flex-col items-center my-5">
       {/* Pick Up */}
-      <div className="w-full h-36 bg-white rounded-lg">Pick Up</div>
+      <div className="w-full h-36 bg-white rounded-lg -z-10">Pick Up</div>
 
-      <button className="bg-primary-500 rounded-lg w-fit p-4">
+      <button className="bg-primary-500 rounded-lg w-fit p-4 absolute">
         <PickDropIcon />
+        <div className="bg-primary-500/40 w-full h-full rounded-lg p-4 absolute z-[-1] right-0 bottom-0 blur-md"></div>
       </button>
 
       {/* Drop off*/}
-      <div className="w-full h-36 bg-white rounded-lg">Drop off</div>
+      <div className="w-full h-36 bg-white rounded-lg -z-10">Drop off</div>
     </section>
   );
 };
