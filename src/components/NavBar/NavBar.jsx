e1e5c125e1060271de0d3860efdd771c32bcc036
Sina Moradian <sinacsharp@gmail.com>
2023-08-26 11:48:06 +0330

    Create NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import FavoritesIcon from "../Icons/FavoritesIcon";
+import NotificationsIcon from "../Icons/NotificationsIcon";
+import ProfileIcon from "../Icons/ProfileIcon";
+import SettingsIcon from "../Icons/SettingsIcon";
+
+const NavBar = () => {
+  return (
+    <div className="flex items-center gap-5 h-fit">
+      <span className="p-2 border rounded-full cursor-pointer">
+        <FavoritesIcon />
+      </span>
+      <span className="p-2 border rounded-full cursor-pointer">
+        <NotificationsIcon />
+      </span>
+      <span className="p-2 border rounded-full cursor-pointer">
+        <SettingsIcon />
+      </span>
+      <span className="w-11 h-11 cursor-pointer">
+        <ProfileIcon />
+      </span>
+    </div>
+  );
+};
+
+export default NavBar;
