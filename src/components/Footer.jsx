e2014ef908b497b2cdb64e68daf54f3f1db0381e
Sina Moradian <sinacsharp@gmail.com>
2023-08-26 12:12:33 +0330

    Add md styles to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,12 +23,14 @@ const Footer = () => {
   ];
 
   return (
-    <footer className="p-5">
+    <footer className="p-5 md:px-10 md:bg-white">
       {/* topper section (Logo + Links) */}
-      <div className="flex flex-col gap-10 mb-10">
-        <div className="w-[220px]">
-          <h2 className="font-bold text-primary-500 text-[24px]">Rentina</h2>
-          <p className="font-medium text-secondary-300 text-[12px]">
+      <div className="flex flex-col md:flex-row md:justify-between border-b-[1px] border-[#13131329] gap-10 mb-10 py-10">
+        <div className="w-[220px] md:w-[292px] flex flex-col gap-4">
+          <h2 className="font-bold text-primary-500 text-2xl md:text-[32px]">
+            Rentina
+          </h2>
+          <p className="font-medium text-secondary-300 text-xs md:text-base">
             Our vision is to provide convenience and help increase your sales
             business.
           </p>
@@ -37,14 +39,16 @@ const Footer = () => {
         {/* Links */}
         <div className="flex gap-10 flex-wrap justify-between">
           {/*---ABOUT---*/}
-          <div className="flex flex-col gap-3">
-            <h2 className="mb-2 text-secondary-500 font-semibold">About</h2>
+          <div className="flex flex-col gap-3 md:gap-5 md:w-40">
+            <h2 className="mb-2 text-secondary-500 font-semibold md:text-xl">
+              About
+            </h2>
             {about.map((item) => {
               return (
                 <Link
                   key={item.id}
                   to={item.path}
-                  className="text-secondary-300 text-[16px]"
+                  className="text-secondary-300 text-base"
                 >
                   {item.name}
                 </Link>
@@ -52,14 +56,16 @@ const Footer = () => {
             })}
           </div>
           {/*---Socials---*/}
-          <div className="flex flex-col gap-3">
-            <h2 className="mb-2 text-secondary-500 font-semibold">Socials</h2>
+          <div className="flex flex-col gap-3 md:gap-5 order-2 md:w-40">
+            <h2 className="mb-2 text-secondary-500 font-semibold md:text-xl">
+              Socials
+            </h2>
             {socials.map((item) => {
               return (
                 <Link
                   key={item.id}
                   to={item.path}
-                  className="text-secondary-300 text-[16px]"
+                  className="text-secondary-300 text-base"
                 >
                   {item.name}
                 </Link>
@@ -67,14 +73,16 @@ const Footer = () => {
             })}
           </div>
           {/*---Community---*/}
-          <div className="flex flex-col gap-3">
-            <h2 className="mb-2 text-secondary-500 font-semibold">Community</h2>
+          <div className="flex flex-col gap-3 md:gap-5 order-1 md:w-40">
+            <h2 className="mb-2 text-secondary-500 font-semibold md:text-xl">
+              Community
+            </h2>
             {community.map((item) => {
               return (
                 <Link
                   key={item.id}
                   to={item.path}
-                  className="text-secondary-300 text-[16px]"
+                  className="text-secondary-300 text-base text-base"
                 >
                   {item.name}
                 </Link>
@@ -84,12 +92,12 @@ const Footer = () => {
         </div>
       </div>
       {/* bottom section */}
-      <div className="flex flex-col gap-5 text-[12px] font-semibold">
-        <div className="flex justify-between">
+      <div className="flex flex-col md:flex-row md:justify-between gap-5 text-xs md:text-base font-semibold pb-10">
+        <div className="flex justify-between md:order-2 md:gap-12">
           <Link to={"."}>Privacy & Policy</Link>
           <Link to={"."}>Terms & Condition</Link>
         </div>
-        <p>©2023 Rentina. All rights reserved</p>
+        <p className="md:order-1">©2023 Rentina. All rights reserved</p>
       </div>
     </footer>
   );
