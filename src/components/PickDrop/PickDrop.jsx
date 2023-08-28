c6ab69513bf49775963797d3d0fa431be0946bd4
Sina Moradian <sinacsharp@gmail.com>
2023-08-28 09:48:03 +0330

    Fix position of the  button in between

--- src/components/PickDrop/PickDrop.jsx
+++ src/components/PickDrop/PickDrop.jsx
@@ -10,7 +10,7 @@ const PickDrop = () => {
     { id: 3, name: "Lam", value: "lambo" },
   ];
   return (
-    <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-6 items-center my-5 relative">
+    <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-6 items-center my-6 relative">
       {/* Pick Up */}
       <div className="flex flex-col justify-between w-full h-32 bg-white rounded-lg z-[1] p-3 gap-3 max-w-lg">
         <div className="flex items-center gap-3">
@@ -40,7 +40,7 @@ const PickDrop = () => {
         </div>
       </div>
 
-      <button className="bg-primary-500 rounded-lg w-fit p-4 absolute top-[40%] md:top-[30%] md:left-0 md:right-0 md:mx-auto z-[999]">
+      <button className="bg-primary-500 rounded-lg w-fit p-4 absolute top-[43%] md:top-[30%] md:left-0 md:right-0 md:mx-auto z-[999]">
         <PickDropIcon />
         <div className="bg-primary-500/40 w-full h-full rounded-lg p-4 absolute z-[-1] right-0 bottom-0 blur-md"></div>
       </button>
