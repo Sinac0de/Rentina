c042d3baeed0ad5f50df3248cbe36572381f2df0
Sina Moradian <sinacsharp@gmail.com>
2023-08-28 09:48:27 +0330

    Remove default padding for main

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -18,7 +18,7 @@ const Layout = () => {
         onClick={() => !isNavCollapsed && setIsNavCollapsed(true)}
       >
         <Header setIsNavCollapsed={setIsNavCollapsed} />
-        <main className="min-h-screen md:p-10 px-7">
+        <main className="min-h-screen">
           <Outlet />
         </main>
         <Footer />
