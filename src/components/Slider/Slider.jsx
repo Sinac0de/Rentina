7eca56fc5cc7edd3ec232f2881a22dd09ce457e9
Sina Moradian <sinacsharp@gmail.com>
2023-08-28 10:56:32 +0330

    Add slides with swiper js

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,23 +1,56 @@
 import { Link } from "react-router-dom";
 import CarCard from "../CarCard/CarCard";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Slider = ({ title }) => {
   return (
     <section>
       <header className="flex justify-between mb-5">
         <h3>{title}</h3>
-        <Link to="." className="text-primary-500 font-semibold">
+        <Link to="/shop" className="text-primary-500 font-semibold">
           <p>View all</p>
         </Link>
       </header>
       {/* slides */}
-      <div className="flex overflow-auto gap-5 relative">
-        <CarCard isSlideCard={true} />
-        <CarCard isSlideCard={true} />
-        <CarCard isSlideCard={true} />
-        <CarCard isSlideCard={true} />
-        <CarCard isSlideCard={true} />
-      </div>
+      <Swiper
+        grabCursor={true}
+        spaceBetween="30"
+        slidesPerView={"auto"}
+        showsPagination={false}
+        modules={[Pagination]}
+        className="mySwiper "
+      >
+        <SwiperSlide>
+          <CarCard isSlideCard={true} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CarCard isSlideCard={true} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CarCard isSlideCard={true} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CarCard isSlideCard={true} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CarCard isSlideCard={true} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CarCard isSlideCard={true} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CarCard isSlideCard={true} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CarCard isSlideCard={true} />
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 };
