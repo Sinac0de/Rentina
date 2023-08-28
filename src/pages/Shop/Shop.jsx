2c5b41d34028865006e7bf99d31a285678d273eb
Sina Moradian <sinacsharp@gmail.com>
2023-08-28 10:57:17 +0330

    Resturcture the shop page

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -2,11 +2,12 @@ import PickDrop from "../../components/PickDrop/PickDrop";
 
 const Shop = () => {
   return (
-    <div className="flex">
-      <div>FilterSidebar</div>
-      <div className="flex-1">
+    <div className="flex min-h-screen">
+      <div className="hidden w-1/4 bg-white">FilterSidebar</div>
+      <div className="flex-1 overflow-y-auto px-5">
         <PickDrop />
         <div>Shop Items</div>
+        <div></div>
       </div>
     </div>
   );
