d53f8c4e9f09a90bfd8d887ee35609bb799dd75d
Sina Moradian <sinacsharp@gmail.com>
2023-08-28 09:49:39 +0330

    Add base structure for the side filter bar

---
+++ src/components/FilterSideBar/FilterSideBar.jsx
@@ -0,0 +1,19 @@
+const FilterSidebar = () => {
+  return (
+    <div>
+      <div>
+        <h3>TYPE</h3>
+        <ul>{/* checkboxes */}</ul>
+      </div>
+      <div>
+        <h3>CAPACITY</h3>
+        <ul>{/* checkboxes */}</ul>
+      </div>
+      <div>
+        <h3>PRICE</h3>
+      </div>
+    </div>
+  );
+};
+
+export default FilterSidebar;
