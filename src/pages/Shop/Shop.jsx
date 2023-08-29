dccbe98417f8691a541f2797d512d9d662271154
Sina Moradian <sinacsharp@gmail.com>
2023-08-29 23:34:08 +0330

    Add Filter component

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,9 +1,12 @@
+import FilterSidebar from "../../components/FilterSideBar/FilterSideBar";
 import PickDrop from "../../components/PickDrop/PickDrop";
 
 const Shop = () => {
   return (
     <div className="flex min-h-screen">
-      <div className="hidden w-1/4 bg-white">FilterSidebar</div>
+      <div className="bg-white p-5 px-10 w-[20%]">
+        <FilterSidebar />
+      </div>
       <div className="flex-1 overflow-y-auto px-5">
         <PickDrop />
         <div>Shop Items</div>
