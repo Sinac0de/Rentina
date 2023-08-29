27ae1d5c30f3e15cd2f812d96879ed499825e202
Sina Moradian <sinacsharp@gmail.com>
2023-08-29 23:57:29 +0330

    Add All Cars List and Add sticky position to the filter

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,16 +1,18 @@
+import AllCarsList from "../../components/AllCarsList/AllCarsList";
 import FilterSidebar from "../../components/FilterSideBar/FilterSideBar";
 import PickDrop from "../../components/PickDrop/PickDrop";
 
 const Shop = () => {
   return (
-    <div className="flex min-h-screen">
-      <div className="bg-white p-5 px-10 w-[20%]">
+    <div className="flex min-h-screen relative">
+      <div className="bg-white p-5 px-10 w-[20%] sticky h-full top-0 z-20">
         <FilterSidebar />
       </div>
       <div className="flex-1 overflow-y-auto px-5">
         <PickDrop />
-        <div>Shop Items</div>
-        <div></div>
+        <div>
+          <AllCarsList />
+        </div>
       </div>
     </div>
   );
