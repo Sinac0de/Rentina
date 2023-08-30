af6513f9aaff8a73597e3e5d3d73ff9772c9ecd5
Sina Moradian <sinacsharp@gmail.com>
2023-08-30 18:29:14 +0330

    Add Styles and improvement to the headers in desktop mode

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,31 @@
-import CarCard from "../../components/CarCard/CarCard";
 import circlePatternUrl from "../../assets/images/Patterns/circularPattern.png";
 import heroCarImageUrl from "../../assets/images/Cars/Koenigsegg-Sport.png";
+import heroCar2ImageUrl from "../../assets/images/Cars/Nissan_GT-R.png";
 import PickDrop from "../../components/PickDrop/PickDrop";
 import Slider from "../../components/Slider/Slider";
 import { Link } from "react-router-dom";
+import AllCarsList from "../../components/AllCarsList/AllCarsList";
 
 const Home = () => {
   return (
-    <div className="px-5">
+    <div className="px-5 md:px-14 md:pt-8">
       {/* Hero */}
       <div className="flex gap-7">
         {/* behind the hero (white background) */}
-        <div className="bg-white h-[15%] w-full absolute -z-10 left-0"></div>
+        <div className="bg-white h-[15%] w-full absolute -z-10 left-0 md:hidden"></div>
 
-        <div className="bg-[#54A6FF] w-full rounded-lg flex justify-center items-center relative min-h-[260px] md:max-h-96">
+        <div className="bg-[#54A6FF] w-full rounded-lg flex justify-center items-center relative min-h-[260px] md:min-h-[360px]">
           <img
             src={circlePatternUrl}
-            className="absolute left-0 bottom-0 right-0"
+            className="absolute left-0 bottom-0 right-0 w-full"
           />
-          <div className="absolute h-full p-5 text-white flex flex-col items-start justify-between">
+          <div className="absolute w-full h-full p-5 text-white flex flex-col items-start justify-between md:justify-normal md:gap-6">
             {/* hero header */}
-            <div className="flex flex-col w-fit gap-2">
-              <h3 className="text-base font-semibold">
+            <div className="flex flex-col md:max-w-[284px] gap-2">
+              <h3 className="text-base font-semibold md:text-[32px] md:font-medium leading-10">
                 The Best Platform for Car Rental
               </h3>
-              <h5 className="text-xs w-4/5">
+              <h5 className="text-xs md:text-base ">
                 Ease of doing a car rental safely and reliably. Of course at a
                 low price.
               </h5>
@@ -32,17 +33,39 @@ const Home = () => {
             {/* hero footer */}
             <Link
               to="/shop"
-              className="bg-primary-500 py-3 px-[20px] rounded-[4px] text-xs font-medium"
+              className="bg-primary-500 py-3 px-[20px] rounded-[4px] text-xs md:text-base font-medium"
             >
               Go to store
             </Link>
-            <div className="w-full px-4 flex justify-center">
-              <img src={heroCarImageUrl} className="w-full" />
+            <div className="w-full px-4 flex justify-center md:absolute md:bottom-4">
+              <img src={heroCarImageUrl} className="w-fit" />
             </div>
           </div>
         </div>
-        <div className="hidden md:block bg-[#ef3c3c] w-full border rounded-lg flex justify-center items-center">
-          <img src="" alt="" />
+        <div className="hidden md:block bg-primary-500 w-full border rounded-lg justify-center items-center relative">
+          <div className="h-full w-full background absolute"></div>
+          <div className="absolute w-full h-full p-5 text-white flex flex-col items-start justify-between md:justify-normal md:gap-6">
+            {/* hero header */}
+            <div className="flex flex-col md:max-w-[284px] gap-2">
+              <h3 className="text-base font-semibold md:text-[32px] md:font-medium leading-10">
+                The Best Platform for Car Rental
+              </h3>
+              <h5 className="text-xs md:text-base ">
+                Ease of doing a car rental safely and reliably. Of course at a
+                low price.
+              </h5>
+            </div>
+            {/* hero footer */}
+            <Link
+              to="/shop"
+              className="bg-[#54A6FF] py-3 px-[20px] rounded-[4px] text-xs md:text-base font-medium"
+            >
+              Go to store
+            </Link>
+            <div className="w-full px-4 flex justify-center md:absolute md:bottom-4">
+              <img src={heroCar2ImageUrl} className="w-fit" />
+            </div>
+          </div>
         </div>
       </div>
 
@@ -55,32 +78,7 @@ const Home = () => {
           <Slider title={"Popular cars"} />
         </div>
         {/* Recommended cars*/}
-
-        <div>
-          <header className="my-5">
-            <h3>Recommended cars</h3>
-          </header>
-          {/* recommended car cards */}
-          <div className="grid grid-flow-row gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-5">
-            <CarCard />
-            <CarCard />
-            <CarCard />
-            <CarCard />
-            <CarCard />
-            <CarCard />
-            <CarCard />
-            <CarCard />
-            <CarCard />
-          </div>
-          <footer className="flex justify-center items-center relative">
-            <button className="bg-primary-500 py-2 px-[20px] rounded-[4px] text-xs">
-              Show More Cars
-            </button>
-            <h3 className="font-bold text-secondary-300 absolute text-sm right-5">
-              120 cars
-            </h3>
-          </footer>
-        </div>
+        <AllCarsList />
       </div>
     </div>
   );
