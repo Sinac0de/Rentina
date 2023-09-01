7eb777056282b47fd116bb82bf76e942a57f9529
Sina Moradian <sinacsharp@gmail.com>
2023-09-01 17:48:24 +0330

    Add child routes to the shop page

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,18 @@ import {
 import Home from "./pages/Home/Home";
 import Shop from "./pages/Shop/Shop";
 import Layout from "./layout/Layout";
+import RentalCars from "./pages/Shop/RentalCars";
+import CarDetails from "./pages/Shop/CarDetails";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path="shop" element={<Shop />}>
+          <Route index element={<RentalCars />} />
+          <Route path=":id" element={<CarDetails />} />
+        </Route>
       </Route>
     )
   );
