553bffe5d0e1041d196bf779f984dd0ab7a187af
Sina Moradian <sinacsharp@gmail.com>
2023-09-02 23:59:20 +0330

    Fix z index of the filter section

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -4,7 +4,7 @@ import FilterSidebar from "../../components/FilterSideBar/FilterSideBar";
 const Shop = () => {
   return (
     <div className="flex relative">
-      <div className="hidden md:block bg-white p-5 px-10 w-[20%] z-10">
+      <div className="hidden md:block bg-white p-5 px-10 w-[20%]">
         <FilterSidebar />
       </div>
       <div className="flex-1 overflow-y-auto px-5">
