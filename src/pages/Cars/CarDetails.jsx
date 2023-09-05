df23950c52a279baf71fc7f7207a14a376128c8f
Sina Moradian <sinacsharp@gmail.com>
2023-09-05 19:00:50 +0330

    Add styles of car images & specs for large screen

--- src/pages/Cars/CarDetails.jsx
+++ src/pages/Cars/CarDetails.jsx
@@ -7,9 +7,9 @@ import smallImg3 from "../../assets/images/Cars/test/car-img-3.png";
 
 const CarDetails = () => {
   return (
-    <div className="pt-8 flex flex-col gap-7">
+    <div className="pt-8 flex flex-col gap-7 lg:flex-row">
       {/* car images */}
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-6 lg:w-1/2">
         <img src={bigImg} className="w-full" />
         <div className="grid grid-cols-3 grid-rows-1 w-full gap-5 h-16">
           {/* active image */}
@@ -35,45 +35,47 @@ const CarDetails = () => {
       </div>
 
       {/* car info */}
-      <div className="flex flex-col gap-5 bg-white p-4 rounded-[10px]">
+      <div className="flex flex-col gap-5 bg-white p-4 rounded-[10px] lg:w-1/2 lg:justify-around">
         {/* info header */}
-        <div className="flex flex-col gap-1">
-          <h2 className="text-secondary-500 text-xl font-bold">
+        <div className="flex flex-col gap-1 lg:gap-3">
+          <h2 className="text-secondary-500 text-xl lg:text-[32px] font-bold">
             Nissan GT - R
           </h2>
           <div className="flex gap-2 items-center">
-            <div className="flex gap-1">
+            <div className="flex gap-1 items-center">
               <StarFilledIcon />
               <StarFilledIcon />
               <StarFilledIcon />
               <StarFilledIcon />
               <StarOutlineIcon />
             </div>
-            <h4 className="text-xs text-secondary-300">440+ Reviewer</h4>
+            <h4 className="text-xs text-secondary-300 lg:text-[14px]">
+              440+ Reviewer
+            </h4>
           </div>
         </div>
         {/* info body */}
         <div className="flex flex-col gap-5">
-          <p className="text-secondary-300 text-xs font-normal">
+          <p className="text-secondary-300 text-xs lg:text-xl lg:text-secondary-400 font-normal">
             NISMO has become the embodiment of Nissan's outstanding performance,
             inspired by the most unforgiving proving ground, the "race track".
           </p>
           {/* car specs */}
-          <div className="flex justify-between gap-4 items-center">
+          <div className="flex justify-between gap-4 items-center lg:gap-5">
             <div className="flex flex-col gap-4 w-1/2">
               <div className="flex justify-between items-center">
-                <h3 className="text-secondary-300 font-medium text-xs">
+                <h3 className="text-secondary-300 font-medium text-xs lg:font-normal lg:text-xl">
                   Type Car
                 </h3>
-                <span className="text-secondary-500 font-semibold text-xs">
+                <span className="text-secondary-500 font-semibold text-xs lg:font-normal lg:text-xl">
                   Sport
                 </span>
               </div>
               <div className="flex justify-between items-center">
-                <h3 className="text-secondary-300 font-medium text-xs">
+                <h3 className="text-secondary-300 font-medium text-xs lg:font-normal lg:text-xl">
                   Steering
                 </h3>
-                <span className="text-secondary-500 font-semibold text-xs">
+                <span className="text-secondary-500 font-semibold text-xs lg:font-normal lg:text-xl">
                   Manual
                 </span>
               </div>
@@ -81,18 +83,18 @@ const CarDetails = () => {
 
             <div className="flex flex-col gap-4 w-1/2">
               <div className="flex justify-between items-center">
-                <h3 className="text-secondary-300 font-medium text-xs">
+                <h3 className="text-secondary-300 font-medium text-xs lg:font-normal lg:text-xl">
                   Capacity
                 </h3>
-                <span className="text-secondary-500 font-semibold text-xs">
+                <span className="text-secondary-500 font-semibold text-xs lg:font-normal lg:text-xl">
                   2 Person
                 </span>
               </div>
               <div className="flex justify-between items-center">
-                <h3 className="text-secondary-300 font-medium text-xs">
+                <h3 className="text-secondary-300 font-medium text-xs lg:font-normal lg:text-xl">
                   Gasoline
                 </h3>
-                <span className="text-secondary-500 font-semibold text-xs">
+                <span className="text-secondary-500 font-semibold text-xs lg:font-normal lg:text-xl">
                   70 L
                 </span>
               </div>
@@ -104,18 +106,18 @@ const CarDetails = () => {
           <div className="flex items-center justify-between gap-2">
             {/* price and discount */}
             <div>
-              <h3 className="font-semibold text-xl text-secondary-500">
+              <h3 className="font-semibold text-xl text-secondary-500 lg:text-2xl">
                 $74.00/
-                <span className="font-medium text-xs text-secondary-300">
+                <span className="font-medium text-xs text-secondary-300 lg:text-base">
                   day
                 </span>
               </h3>
-              <h4 className="text-xs text-secondary-300 line-through">
+              <h4 className="text-xs text-secondary-300 line-through lg:text-base">
                 $80.00
               </h4>
             </div>
 
-            <button className="bg-primary-500 py-2 px-[20px] h-full rounded text-base font-medium">
+            <button className="bg-primary-500 py-2 px-[20px] h-full rounded text-base lg:py-4 font-medium">
               Rent now
             </button>
           </div>
