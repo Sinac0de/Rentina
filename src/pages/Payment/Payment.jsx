d87b4f90d1d6e036f141131813fa4f2ea92530b0
Sina Moradian <sinacsharp@gmail.com>
2023-09-07 22:47:56 +0330

    Add Payment page's base structure

---
+++ src/pages/Payment/Payment.jsx
@@ -0,0 +1,17 @@
+const Payment = () => {
+  return (
+    <div>
+      <div>{/* Rental summary */}</div>
+
+      <div>{/* Billing Info */}</div>
+
+      <div>{/* Rental Info */}</div>
+
+      <div>{/* Payment Method */}</div>
+
+      <div>{/* Confirmation */}</div>
+    </div>
+  );
+};
+
+export default Payment;
