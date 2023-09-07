51ea32f916cce15cd5434c662bd8eb0b52cc9008
Sina Moradian <sinacsharp@gmail.com>
2023-09-07 23:49:32 +0330

    Refactor Relative paths

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,18 @@ import Layout from "./layout/Layout";
 import RentalCars from "./pages/Cars/RentalCars";
 import CarDetails from "./pages/Cars/CarDetails/CarDetails";
 import Cars from "./pages/Cars/Cars";
+import Payment from "./pages/Payment/Payment";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/shop" element={<Cars />}>
+        <Route path="shop" element={<Cars />}>
           <Route index element={<RentalCars />} />
-          <Route path="/shop/:id" element={<CarDetails />} />
+          <Route path=":id" element={<CarDetails />} />
         </Route>
+        <Route path="payment/:id" element={<Payment />} />
         <Route path="*" element={<h1>Page not found!</h1>} />
       </Route>
     )
