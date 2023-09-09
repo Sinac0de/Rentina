fa44807d8f8fb0df99a711d712b7b5ce5a9f53fd
Sina Moradian <sinacsharp@gmail.com>
2023-09-10 00:31:46 +0330

    Restructure the layout

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -3,16 +3,14 @@ import RentalSummary from "./Parts/RentalSummary";
 
 const Payment = () => {
   return (
-    <div className="p-5">
-      <RentalSummary />
+    <div className="p-5 md:p-10 flex flex-col gap-6 md:flex-row justify-center">
+      <div className="md:order-2 lg:w-1/3 md:2/5">
+        <RentalSummary />
+      </div>
 
-      <BillingInfo />
-
-      <div>{/* Rental Info */}</div>
-
-      <div>{/* Payment Method */}</div>
-
-      <div>{/* Confirmation */}</div>
+      <div className="md:order-1 md:w-3/5 flex flex-col gap-6 w-full">
+        <BillingInfo />
+      </div>
     </div>
   );
 };
