4cf82953c4209a91ad59321d8fa312fc6328b937
Sina Moradian <sinacsharp@gmail.com>
2023-09-11 23:36:51 +0330

    Add RentalInfo component

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,4 +1,5 @@
 import BillingInfo from "./Parts/BillingInfo";
+import RentalInfo from "./Parts/RentalInfo";
 import RentalSummary from "./Parts/RentalSummary";
 
 const Payment = () => {
@@ -10,6 +11,7 @@ const Payment = () => {
 
       <div className="md:order-1 md:w-3/5 flex flex-col gap-6 w-full">
         <BillingInfo />
+        <RentalInfo />
       </div>
     </div>
   );
