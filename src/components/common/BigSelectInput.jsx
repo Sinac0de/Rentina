c614a1c769065ae6074638de2ca25b47177c4717
Sina Moradian <sinacsharp@gmail.com>
2023-09-11 23:34:39 +0330

    Fix chevron icon's position

--- src/components/common/BigSelectInput.jsx
+++ src/components/common/BigSelectInput.jsx
@@ -1,18 +1,18 @@
 const BigSelectInput = ({ id, label, options }) => {
   return (
-    <div className="flex flex-col w-full h-fit">
-      <label htmlFor={id} className="font-bold">
+    <div className="flex flex-col w-full h-fit gap-2">
+      <label htmlFor={id} className="font-semibold text-sm">
         {label}
       </label>
       <select
         id={id}
         name={id}
-        className="w-full p-3 bg-[#F6F7F9] rounded-[10px] text-xs placeholder:text-secondary-300 text-secondary-500 focus:ring-1 focus:ring-secondary-300 border-none outline-none"
+        className="w-full p-3 bg-[#F6F7F9] rounded-[10px] text-xs text-secondary-300 focus:ring-1 focus:ring-secondary-300 border-r-[14px] border-transparent px-4 outline-none"
       >
         {options.map((option) => {
           return (
             <option
-              className="text-secondary-300"
+              className="text-secondary-300 text-xs"
               key={option.id}
               value={option.value}
             >
