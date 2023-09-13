f7f881a3370b8e774f8943b338f3ef5597cd93f3
Sina Moradian <sinacsharp@gmail.com>
2023-09-13 23:10:05 +0330

    Refactor fetch photos from url

--- src/pages/Cars/CarDetails/parts/CarPhotos.jsx
+++ src/pages/Cars/CarDetails/parts/CarPhotos.jsx
@@ -1,67 +1,138 @@
-import bigImg from "src/assets/images/Cars/test/car-img-1.png";
-import smallImg2 from "src/assets/images/Cars/test/car-img-2.png";
-import smallImg3 from "src/assets/images/Cars/test/car-img-3.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const CarPhotos = () => {
-  const [activeImg, setActiveImg] = useState({ id: 1, src: bigImg });
+  const [photos, setPhotos] = useState([]);
+  const [activeImg, setActiveImg] = useState({ id: 1, src: "" });
 
   const activeImgHandler = (e) => {
-    setActiveImg({ id: e.target.parentNode.id, src: e.target.src });
+    setActiveImg({ id: e.target.id, src: e.target.src });
   };
+
+  useEffect(() => {
+    fetch("http://localhost:3000/cars/1")
+      .then((res) => res.json())
+      .then((data) => {
+        setPhotos(data.img_urls);
+        setActiveImg(data.img_urls[0]);
+      });
+  }, []);
+
+  if (!photos.length) {
+    // !todo add skeleton loading
+    return <h2>loading...</h2>;
+  }
 
   return (
     <div className="flex flex-col gap-6 h-full lg:w-1/2">
       <img src={activeImg.src} className="w-full object-contain" />
       <div className="grid grid-cols-3 grid-rows-1 w-full gap-5 h-2/5 md:flex-1">
         {/* active image */}
-        <div
-          id="img1"
-          className={`cursor-pointer ${
-            activeImg.id == "img1"
-              ? "p-1 border-[1.5px] border-primary-500 rounded-lg"
-              : ""
-          }`}
-        >
-          <img
-            src={bigImg}
-            className="h-full w-full object-cover object-center rounded-md"
-            onClick={activeImgHandler}
-          />
-        </div>
-
-        <div
-          id="img2"
-          className={`cursor-pointer ${
-            activeImg.id == "img2"
-              ? "p-1 border-[1.5px] border-primary-500 rounded-lg"
-              : ""
-          }`}
-        >
-          <img
-            src={smallImg2}
-            className="h-full w-full object-cover object-center rounded-md"
-            onClick={activeImgHandler}
-          />
-        </div>
-
-        <div
-          id="img3"
-          className={`cursor-pointer ${
-            activeImg.id == "img3"
-              ? "p-1 border-[1.5px] border-primary-500 rounded-lg"
-              : ""
-          }`}
-        >
-          <img
-            src={smallImg3}
-            className="h-full w-full object-cover object-center rounded-md"
-            onClick={activeImgHandler}
-          />
-        </div>
+        {photos.map((photo) => {
+          return (
+            <img
+              key={photo.id}
+              id={photo.id}
+              src={photo.src}
+              className={`h-full w-full object-cover cursor-pointer ${
+                activeImg.id == photo.id
+                  ? "p-1 border-[3px] border-primary-500"
+                  : ""
+              } object-center rounded-md`}
+              onClick={activeImgHandler}
+            />
+          );
+        })}
       </div>
     </div>
   );
+
+  // -----------photos-----------
+  // return (
+  //   <div className="flex flex-col gap-6 h-full lg:w-1/2">
+  //     <img src={photos[0].src} className="w-full object-contain" />
+  //     <div className="grid grid-cols-3 grid-rows-1 w-full gap-5 h-2/5 md:flex-1">
+  //       {/* active image */}
+  //       <div id="img1" className={`cursor-pointer`}>
+  //         <img
+  //           src={photos[0].src}
+  //           className="h-full w-full object-cover object-center rounded-md"
+  //           onClick={activeImgHandler}
+  //         />
+  //       </div>
+
+  //       <div id="img2" className={`cursor-pointer`}>
+  //         <img
+  //           src={photos[1].src}
+  //           className="h-full w-full object-cover object-center rounded-md"
+  //           onClick={activeImgHandler}
+  //         />
+  //       </div>
+
+  //       <div id="img3" className={`cursor-pointer`}>
+  //         <img
+  //           src={photos[2].src}
+  //           className="h-full w-full object-cover object-center rounded-md"
+  //           onClick={activeImgHandler}
+  //         />
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
+
+  // assets/teest
+
+  // return (
+  //   <div className="flex flex-col gap-6 h-full lg:w-1/2">
+  //     <img src={activeImg.src} className="w-full object-contain" />
+  //     <div className="grid grid-cols-3 grid-rows-1 w-full gap-5 h-2/5 md:flex-1">
+  //       {/* active image */}
+  //       <div
+  //         id="img1"
+  //         className={`cursor-pointer ${
+  //           activeImg.id == "img1"
+  //             ? "p-1 border-[1.5px] border-primary-500 rounded-lg"
+  //             : ""
+  //         }`}
+  //       >
+  //         <img
+  //           src={photos[0]}
+  //           className="h-full w-full object-cover object-center rounded-md"
+  //           onClick={activeImgHandler}
+  //         />
+  //       </div>
+
+  //       <div
+  //         id="img2"
+  //         className={`cursor-pointer ${
+  //           activeImg.id == "img2"
+  //             ? "p-1 border-[1.5px] border-primary-500 rounded-lg"
+  //             : ""
+  //         }`}
+  //       >
+  //         <img
+  //           src={photos[1]}
+  //           className="h-full w-full object-cover object-center rounded-md"
+  //           onClick={activeImgHandler}
+  //         />
+  //       </div>
+
+  //       <div
+  //         id="img3"
+  //         className={`cursor-pointer ${
+  //           activeImg.id == "img3"
+  //             ? "p-1 border-[1.5px] border-primary-500 rounded-lg"
+  //             : ""
+  //         }`}
+  //       >
+  //         <img
+  //           src={photos[2]}
+  //           className="h-full w-full object-cover object-center rounded-md"
+  //           onClick={activeImgHandler}
+  //         />
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default CarPhotos;
