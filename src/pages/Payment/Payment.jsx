08be7bdb40886b26076eab5ae236c27de1fed2c6
Sina Moradian <sinacsharp@gmail.com>
2023-09-22 21:37:47 +0330

    Add Fetch Car data for the payment section

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,14 +1,37 @@
+import { getCars } from "src/services/api";
 import BillingInfo from "./Parts/BillingInfo";
 import Confirmation from "./Parts/Confirmation";
 import PaymentMethod from "./Parts/PaymentMethod";
 import RentalInfo from "./Parts/RentalInfo";
 import RentalSummary from "./Parts/RentalSummary";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const Payment = () => {
+  const [carData, setCarData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    // Reset the loading state
+    setIsLoading(true);
+    // fetch car data
+    async function fetchCarDetails() {
+      const data = await getCars(id);
+      setCarData(data);
+      setIsLoading(false); // Set loading to false when data is fetched
+    }
+    fetchCarDetails();
+  }, [id]);
+
+  if (isLoading) {
+    return <h4>Loading car data...</h4>;
+  }
+
   return (
-    <div className="relative p-5 md:p-10 flex flex-col gap-6 md:flex-row justify-center">
-      <div className="md:order-2 lg:w-2/3 md:1/5 sticky top-36 h-fit">
-        <RentalSummary />
+    <div className="relative p-5 md:p-10 flex flex-col gap-6 lg:flex-row justify-center">
+      <div className="lg:order-2 lg:w-2/3 md:w-full lg:sticky top-36 h-fit">
+        <RentalSummary info={carData} />
       </div>
 
       <div className="md:order-1 flex flex-col gap-6 w-full">
