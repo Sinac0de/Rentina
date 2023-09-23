9ca6003802b697ce192769a4e1432518ebc76995
Sina Moradian <sinacsharp@gmail.com>
2023-09-23 18:08:07 +0330

    Add vertical margins and cursor pointer to the select

--- src/components/common/BigSelectInput.jsx
+++ src/components/common/BigSelectInput.jsx
@@ -1,13 +1,13 @@
 const BigSelectInput = ({ id, label, options }) => {
   return (
-    <div className="flex flex-col w-full h-fit gap-2">
+    <div className="flex flex-col w-full h-fit gap-2 my-5 lg:my-0">
       <label htmlFor={id} className="font-semibold text-sm lg:text-base">
         {label}
       </label>
       <select
         id={id}
         name={id}
-        className="w-full p-4 bg-[#F6F7F9] rounded-[10px] text-xs text-secondary-300 focus:ring-1 focus:ring-secondary-300 border-r-[14px] border-transparent px-4 outline-none lg:text-sm"
+        className="w-full p-4 bg-[#F6F7F9] rounded-[10px] text-xs text-secondary-300 focus:ring-1 focus:ring-secondary-300 border-r-[14px] border-transparent px-4 outline-none lg:text-sm cursor-pointer"
       >
         {options.map((option) => {
           return (
