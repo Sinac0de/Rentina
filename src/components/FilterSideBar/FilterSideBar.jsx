659076383c785b7ed9cf7d0a4f8dc684673a15b0
Sina Moradian <sinacsharp@gmail.com>
2023-09-25 17:53:57 +0330

    Add fetch types, seating, and price data

--- src/components/FilterSideBar/FilterSideBar.jsx
+++ src/components/FilterSideBar/FilterSideBar.jsx
@@ -1,105 +1,103 @@
 import Checkbox from "../common/Checkbox";
 import RangeInput from "../common/RangeInput";
+import { useState, useEffect } from "react";
+import { getCarsSpecs } from "src/services/api";
+import SkeletonFilters from "./SkeletonFilters";
 
 const FilterSidebar = () => {
-  // fake data
-  const types = [
-    {
-      id: 1,
-      type: "Sport",
-      count: 10,
-    },
-    {
-      id: 2,
-      type: "SUV",
-      count: 12,
-    },
-    {
-      id: 3,
-      type: "MPV",
-      count: 16,
-    },
-    {
-      id: 4,
-      type: "Sedan",
-      count: 20,
-    },
-    {
-      id: 5,
-      type: "Coupe",
-      count: 14,
-    },
-    {
-      id: 6,
-      type: "Hatchback",
-      count: 14,
-    },
-  ];
+  const [types, setTypes] = useState([]);
+  const [seats, setSeats] = useState([]);
+  const [prices, setPrices] = useState([]);
 
-  const capacities = [
-    {
-      id: 1,
-      capacity: "2 Person",
-      count: 10,
-    },
-    {
-      id: 2,
-      capacity: "4 Person",
-      count: 12,
-    },
-    {
-      id: 3,
-      capacity: "6 Person",
-      count: 16,
-    },
-    {
-      id: 4,
-      capacity: "8 or More",
-      count: 20,
-    },
-  ];
+  useEffect(() => {
+    const allTypes = [];
+    const allSeats = [];
+    const allPrices = [];
+
+    /* ---Get All Specs of cars--- */
+    async function fetchCarsSpecs() {
+      const data = await getCarsSpecs();
+
+      data.forEach((specs) => {
+        /* ---Gather types--- */
+        const typeData = {
+          count: data.filter((data) => data.type === specs.type).length,
+          typeName: specs.type,
+        };
+        if (!allTypes.some((item) => item.typeName === specs.type)) {
+          allTypes.push(typeData);
+        }
+
+        /* ---Gather Seats--- */
+        const seatsData = {
+          count: data.filter((data) => data.seats === specs.seats).length,
+          seats: `${specs.seats} seats`,
+        };
+        if (!allSeats.some((item) => item.seats === `${specs.seats} seats`)) {
+          allSeats.push(seatsData);
+        }
+
+        /* ---Gather Prices--- */
+        const pricesData = specs.rental_price;
+        console.log(specs.rental_price);
+        if (!allPrices.some((price) => price === specs.rental_price)) {
+          allPrices.push(pricesData);
+        }
+      });
+
+      setTypes(allTypes);
+      setSeats(allSeats);
+      setPrices(allPrices);
+    }
+    fetchCarsSpecs();
+  }, []);
+
+  /* ---Skeleton Loading--- */
+  if (!types.length) {
+    return <SkeletonFilters />;
+  }
 
   return (
     <>
       <div>
-        {/* Type */}
+        {/* ---Type--- */}
         <h3 className="text-xs text-secondary-300 tracking-widest">TYPE</h3>
         {/* checkboxes */}
         <ul className="flex flex-col gap-4 text-xl my-5 mb-10">
-          {types.map((type) => {
+          {types.map((type, index) => {
             return (
               <Checkbox
-                id={type.id}
-                key={type.id}
-                label={type.type}
+                id={index}
+                key={index}
+                label={type.typeName}
                 count={type.count}
               />
             );
           })}
         </ul>
       </div>
-      {/* Capacity */}
+      {/* ---Seating--- */}
       <div>
-        <h3 className="text-xs text-secondary-300 tracking-widest">CAPACITY</h3>
+        <h3 className="text-xs text-secondary-300 tracking-widest">Seating</h3>
         {/* checkboxes */}
         <ul className="flex flex-col gap-4 text-xl my-5 mb-10">
-          {capacities.map((capacity) => {
+          {seats.map((seats, index) => {
             return (
               <Checkbox
-                id={capacity.id}
-                key={capacity.id}
-                label={capacity.capacity}
-                count={capacity.count}
+                id={index}
+                key={index}
+                label={seats.seats}
+                count={seats.count}
               />
             );
           })}
         </ul>
       </div>
-      {/* Price */}
+      {/* ---Price--- */}
       <div>
         <h3 className="text-xs text-secondary-300 tracking-widest">PRICE</h3>
         <div className="my-5 mb-10">
-          <RangeInput />
+          <RangeInput max={Math.max(...prices)} />
         </div>
       </div>
     </>
