e0cd493c336d1fa2891bdf2aa4a641d3268b247e
Sina Moradian <sinacsharp@gmail.com>
2023-09-26 21:03:14 +0330

    Create Skeleton loading for single Car's Details page

---
+++ src/pages/Cars/CarDetails/parts/CarReviews/parts/SkeletonReview.jsx
@@ -0,0 +1,33 @@
+import ProfileIcon from "src/components/Icons/ProfileIcon";
+
+const SkeletonReview = () => {
+  return (
+    <div className="flex items-start gap-2">
+      <span className="p-2 border rounded-full cursor-pointer">
+        <ProfileIcon />
+      </span>
+      <div className="flex flex-col gap-3 w-full">
+        {/* comment header */}
+        <div className="flex justify-between">
+          <div className="flex flex-col justify-between">
+            <div className="w-28 h-3 bg-primary-300 mb-1 rounded-full"></div>
+            <div className="w-20 h-3 bg-[#EFF3FD] mb-1 rounded-full"></div>
+          </div>
+          <div className="flex flex-col justify-between items-end pt-1">
+            <div className="w-20 h-3 bg-[#EFF3FD] mb-1 rounded-full"></div>
+            <div>
+              <div className="w-20 h-3 bg-[#EFF3FD] mb-1 rounded-full"></div>
+            </div>
+          </div>
+        </div>
+        {/* comment text */}
+        <div>
+          <div className="w-full h-3 bg-[#EFF3FD] mb-1 rounded-full"></div>
+          <div className="w-full h-3 bg-[#EFF3FD] mb-1 rounded-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SkeletonReview;
---
+++ src/pages/Cars/CarDetails/parts/SkeletonPhotos.jsx
@@ -0,0 +1,28 @@
+import GalleryIcon from "src/components/Icons/GalleryIcon";
+
+const SkeletonPhotos = () => {
+  return (
+    <div className="flex flex-col gap-6 h-full lg:w-1/2 animate-pulse">
+      <div className="w-full h-80 rounded-[10px] bg-white flex justify-center items-center">
+        <GalleryIcon isBig />
+      </div>
+      <div className="grid grid-cols-3 grid-rows-1 w-full gap-5 h-2/5 md:flex-1">
+        {Array.from({ length: 3 }).map((_, index) => {
+          return (
+            <div
+              key={index}
+              id={index}
+              className="h-full w-full flex justify-center items-center rounded-md p-2 border-[3px] border-white"
+            >
+              <div className="bg-white py-5 w-full h-full flex justify-center items-center">
+                <GalleryIcon />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default SkeletonPhotos;
