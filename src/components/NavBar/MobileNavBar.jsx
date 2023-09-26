486ca558f8bfab072b4d1b02109944ecf73be023
Sina Moradian <sinacsharp@gmail.com>
2023-09-27 00:34:03 +0330

    Add Smooth transition to the mobile navbar

--- src/components/NavBar/MobileNavBar.jsx
+++ src/components/NavBar/MobileNavBar.jsx
@@ -11,13 +11,13 @@ const MobileNavBar = ({ isNavCollapsed, setIsNavCollapsed }) => {
           isNavCollapsed
             ? "opacity-0 translate-x-full"
             : "opacity-100 translate-x-0"
-        } w-full h-full fixed right-0 left-0 bottom-0 top-0 z-[998] bg-black/70 transition-opacity`}
+        } w-full h-full fixed right-0 left-0 bottom-0 top-0 z-[998] bg-black/70 transition-opacity duration-500`}
         onClick={() => setIsNavCollapsed(true)}
       ></div>
       <div
         className={`${
           isNavCollapsed ? "translate-x-full" : "translate-x-0"
-        } fixed right-0 bottom-0 bg-white border-l-2 shadow-2xl rounded-l-3xl h-full w-3/4 z-[999] transition-all flex flex-col text-secondary-400 overflow-y-auto`}
+        } fixed right-0 bottom-0 bg-white border-l-2 shadow-2xl rounded-l-3xl h-full w-3/4 z-[999] transition-transform duration-500 flex flex-col text-secondary-400 overflow-y-auto`}
       >
         <div className="flex justify-center items-center h-[15%]">
           <h2 className="font-bold text-primary-500 text-2xl">Rentina</h2>
@@ -41,14 +41,11 @@ const MobileNavBar = ({ isNavCollapsed, setIsNavCollapsed }) => {
         </div>
 
         {/* Nav Footer */}
-        <div className="w-full px-5 py-3 flex flex-col gap-3 h-[23%]">
-          <div className="cursor-pointer flex items-center gap-2">
-            <ProfileIcon />
-            <h3 className="text-base font-semibold">Hi, Sina!</h3>
+        <div className="w-full px-5 py-5 flex flex-col gap-3 h-[23%] justify-end">
+          <div className="p-4 border bg-primary-500 text-white border-secondary-400 rounded-full cursor-pointer flex items-center gap-2">
+            <ProfileIcon color="white" />
+            <h3 className="text-base font-semibold">Sign Up</h3>
           </div>
-          <button className="w-full border border-primary-500 text-primary-500 focus:bg-primary-100/25 p-4 rounded-full mx-auto">
-            Sign Out
-          </button>
         </div>
       </div>
     </>
