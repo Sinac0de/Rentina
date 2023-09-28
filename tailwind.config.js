a622c5cf3be78eb89ad1d0f1196dce9f6d57df0b
Sina Moradian <sinacsharp@gmail.com>
2023-09-28 22:33:59 +0330

    Add basic dark mode configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@
 import withMT from "@material-tailwind/react/utils/withMT";
 
 export default withMT({
+  darkMode: "media",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -15,6 +16,28 @@ export default withMT({
         // => @media (min-width: 576px) { ... }
       },
       backgroundColor: {
+        darkPrimary: {
+          100: "#D6E4FD",
+          200: "#AEC8FC",
+          300: "#85A8F8",
+          400: "#658DF1",
+          500: "#3563E9",
+          600: "#264BC8",
+          700: "#1A37A7",
+          800: "#102587",
+          900: "#0A196F",
+        },
+        darkSecondary: {
+          100: "#E0E9F4",
+          200: "#C3D4E9",
+          300: "#90A3BF",
+          400: "#596780",
+          500: "#1A202C",
+          600: "#131825",
+          700: "#0D121F",
+          800: "#080C19",
+          900: "#040815",
+        },
         primary: {
           100: "#D6E4FD",
           200: "#AEC8FC",
