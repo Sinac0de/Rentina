f7e33facaacbd09d3818141116e38989e47a9444
Sina Moradian <sinacsharp@gmail.com>
2023-09-28 13:41:04 +0330

    Add search by name function

--- src/services/api.js
+++ src/services/api.js
@@ -30,3 +30,19 @@ export async function getCarsSpecs() {
     return carsSpecs;
   }
 }
+/* Search cars by name */
+export async function getCarsByName(name) {
+  const url = "https://api.npoint.io/2748d15519c67fc51dc5/cars";
+  const data = await axios
+    .get(url)
+    .then((res) => res.data)
+    .catch(() => alert("Something went wrong! Please refresh the page."));
+
+  if (data) {
+    const cars = data.filter((car) => {
+      const carName = `${car.make.toLowerCase()} ${car.model.toLowerCase()}`;
+      return carName.includes(name.toLowerCase());
+    });
+    return cars;
+  }
+}
