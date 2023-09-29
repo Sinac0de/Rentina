06ab360c55adca004d75e38cbc2e80b4e06a6920
Sina Moradian <sinacsharp@gmail.com>
2023-09-29 22:21:20 +0330

    Remove placeholder from inputs

--- src/components/common/TimeInput.jsx
+++ src/components/common/TimeInput.jsx
@@ -10,11 +10,10 @@ const TimeInput = ({ id, label, isCompact }) => {
         type="time"
         id={id}
         name={id}
-        placeholder="Select a time..."
         className={`${
           isCompact
-            ? "bg-transparent text-secondary-300 w-full text-sm placeholder:text-sm focus:border-none focus:outline-none"
-            : "w-full py-4 bg-[#F6F7F9] rounded-[10px] text-xs text-secondary-300 focus:ring-1 focus:ring-secondary-300 placeholder:text-secondary-300 border-r-[14px] border-transparent pl-4 outline-none lg:text-sm cursor-pointer"
+            ? "bg-transparent text-secondary-300 w-full text-sm focus:border-none focus:outline-none"
+            : "w-full py-4 bg-[#F6F7F9] rounded-[10px] text-xs text-secondary-300 focus:ring-1 focus:ring-secondary-300 border-r-[14px] border-transparent pl-4 outline-none lg:text-sm cursor-pointer"
         }`}
       />
     </div>
