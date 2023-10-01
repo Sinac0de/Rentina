8841d6073280e06f396fbe661a63113c79b216ce
Sina Moradian <sinacsharp@gmail.com>
2023-10-01 14:04:45 +0330

    Add Dark mode to searchbar and mobile navbar

--- src/components/NavBar/MobileNavBar.jsx
+++ src/components/NavBar/MobileNavBar.jsx
@@ -15,7 +15,7 @@ const MobileNavBar = ({ isNavCollapsed, setIsNavCollapsed }) => {
         onClick={() => setIsNavCollapsed(true)}
       ></div>
       <div
-        className={`${
+        className={`dark:bg-slate-900 dark:border-slate-800 ${
           isNavCollapsed ? "translate-x-full" : "translate-x-0"
         } fixed right-0 bottom-0 bg-white border-l-2 shadow-2xl rounded-l-3xl h-full w-3/4 z-[999] transition-transform duration-500 flex flex-col text-secondary-400 overflow-y-auto`}
       >
@@ -25,15 +25,15 @@ const MobileNavBar = ({ isNavCollapsed, setIsNavCollapsed }) => {
         {/* Nav body */}
         <div className="px-3 flex-1 overflow-y-auto">
           <div className="flex flex-col w-full gap-3">
-            <div className="p-4 border border-secondary-400 rounded-full cursor-pointer flex items-center gap-2">
+            <div className="border-2 p-4 border border-secondary-400 rounded-full cursor-pointer flex items-center gap-2">
               <FavoritesIcon />
               <h3>Favorites</h3>
             </div>
-            <div className="p-4 border border-secondary-400 rounded-full cursor-pointer flex items-center gap-2">
+            <div className="border-2 p-4 border border-secondary-400 rounded-full cursor-pointer flex items-center gap-2">
               <NotificationsIcon />
               <h3>Notifications</h3>
             </div>
-            <div className="p-4 border border-secondary-400 rounded-full cursor-pointer flex items-center gap-2">
+            <div className="border-2 p-4 border border-secondary-400 rounded-full cursor-pointer flex items-center gap-2">
               <SettingsIcon />
               <h3>Settings</h3>
             </div>
@@ -42,7 +42,7 @@ const MobileNavBar = ({ isNavCollapsed, setIsNavCollapsed }) => {
 
         {/* Nav Footer */}
         <div className="w-full px-5 py-5 flex flex-col gap-3 h-[23%] justify-end">
-          <div className="p-4 border bg-primary-500 text-white border-secondary-400 rounded-full cursor-pointer flex items-center gap-2">
+          <div className="dark:border-none p-4 bg-primary-500 text-white border-secondary-400 rounded-full cursor-pointer flex items-center gap-2">
             <ProfileIcon color="white" />
             <h3 className="text-base font-semibold">Sign Up</h3>
           </div>
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -41,11 +41,11 @@ const SearchBar = () => {
         value={searchValue}
         onBlur={() => setShowBox(false)}
         onFocus={() => setShowBox(true)}
-        className="dark:bg-slate-800 dark:border-slate-500 dark:placeholder:text-slate-500 dark:text-slate-500 dark:focus:outline-none w-full relative flex items-center flex-1 gap-2 p-3 pl-14 rounded-lg border md:rounded-full"
+        className="dark:bg-slate-800 dark:border-slate-500 dark:placeholder:text-slate-300 dark:text-slate-300 dark:focus:outline-none w-full relative flex items-center flex-1 gap-2 p-3 pl-14 rounded-lg border md:rounded-full"
       />
       {/* searched items */}
       <div
-        className={`${
+        className={`dark:bg-slate-800 dark:border-slate-500 dark:text-slate-300 ${
           cars.length || showBox ? "" : "hidden"
         } absolute top-12 lg:top-11 left-0 right-0 mx-auto w-[90%] py-4 flex justify-center items-center bg-white overflow-hidden border border-t-0 rounded-b-xl`}
       >
@@ -56,7 +56,7 @@ const SearchBar = () => {
                 <Link
                   to={`/shop/${car.id}`}
                   key={index}
-                  className="w-full flex items-center justify-between h-20 p-5 border-t-2 first:border-none "
+                  className="w-full flex items-center justify-between h-20 p-5 border-t-2 first:border-none"
                   onClick={handleResetSearch}
                 >
                   <div className="h-full w-1/2 p-5 flex items-center">
@@ -72,8 +72,10 @@ const SearchBar = () => {
               );
             })}
           </div>
-        ) : (
+        ) : searchValue ? (
           <h3>Car Not Found!</h3>
+        ) : (
+          ""
         )}
       </div>
     </div>
